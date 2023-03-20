8bd725c9ca439cc1de05be0c5695b13471af0af9
Emre <emrekavak3938@gmail.com>
2023-03-20 11:35:26 +0000

    Update ReadFilmData.java

--- ReadFilmData.java
+++ ReadFilmData.java
@@ -27,20 +27,25 @@ public class ReadFilmData {
 		}
 		sc.close();  //closes the scanner
 		
+		//Testing HighRatedInLimit(float limit)
+		//Film[] see = HighRatedInLimit((float)5);
+		//System.out.println("The movies in the limit we asked: 5");
+		//print(see);
+		
 		//Movie adding test
-		addnewFilm("Spiderman", "Fiction", "Quantin Tarantino", (float)3.19, (float)3.19);
-		addnewFilm("Batman", "Fiction", "Quantin Tarantino", (float)-5, (float)55);
+		//addnewFilm("Spiderman", "Fiction", "Quantin Tarantino", (float)3.19, (float)3.19);
+		//addnewFilm("Batman", "Fiction", "Quantin Tarantino", (float)-5, (float)55);
 		//System.out.println(films[10001]);
 		
-		Film[] films10 = createNew(films, 10);
+		/*Film[] films10 = createNew(films, 10);
 		Film[] films100 = createNew(films, 100);
 		Film[] films1000 = createNew(films, 1000);
 		Film[] films5000 = createNew(films, 5000);
-		
+		*/
 		
 		//Passing the array and copy arrays to mergeSort to see the time complexity
-		double current = System.currentTimeMillis();/*
-		mergeSort(films10);
+		//double current = System.currentTimeMillis();/*
+		/*mergeSort(films10);
 		System.out.println("\n"+"MergeSort 10 data: " + (System.currentTimeMillis()-current));
 		
 		current = System.currentTimeMillis();
@@ -55,13 +60,13 @@ public class ReadFilmData {
 		mergeSort(films5000);
 		System.out.println("MergeSort 5000 data: " + (System.currentTimeMillis()-current));
 		*/
-		current = System.currentTimeMillis();
+		//current = System.currentTimeMillis();
 		mergeSort(films);
-		System.out.println("MergeSort 10000 data: " + (System.currentTimeMillis()-current));
+		//System.out.println("MergeSort 10000 data: " + (System.currentTimeMillis()-current));
 		//System.out.println(films[10001]);
 		
 		
-		//print(films);
+		print(films);
 		/*System.out.println("\nfilms10");
 		print(films10);
 		System.out.println("\nfilms100");
@@ -232,6 +237,7 @@ public class ReadFilmData {
 
 	/*Adding new movie to array, incrementing size +1 to have space for new movie*/
 	public static void addnewFilm(String title, String genre, String directorName, float length, float rating) {
+
 		try {
 			if(length<0) {
 				throw new IncorrectLengthException(title + " length can't be less than zero");
@@ -247,7 +253,88 @@ public class ReadFilmData {
 		}
 	}
 	
-	//public static void addFilm(String genre,)
+	/*Finds the movies with the highest rate within the sum of the lengths in limit
+	 * Calls the quick sort the array with high rate based*/
+	public static Film[] HighRatedInLimit(float limit) {
+		//Film [] ranked = new Film[15];
+		int n = films.length;
+		quickSort(films, 0, n-1); //Calling the quick sort with starting and end index
+		int count = discountLength(limit);
+		Film[] ranked = new Film[count]; //Declaring new Film array with the number indicates how many high rated movie length sum could be in the limit
+		//print(films);
+		ranked = copy(films, count);
+		return ranked;
+	}
+	
+	//to copy the sorted array to new array
+	private static Film[] copy(Film[] flms, int count) {
+		int lastIndex = flms.length-1;
+		//System.out.println(lastIndex);
+		Film[] high = new Film[count];
+		int j = 0; //index number for high
+		//Going from last to last-count
+		for(int i=lastIndex; i>(lastIndex-count); i--) {
+			high[j] = flms[i];
+			j++;
+		}
+		//print(high);
+		return high;
+		
+	}
+
+	public static void quickSort(Film[] arr, int low, int high) {
+		if(low<high) {
+			//pi is the pivot to divide the array
+			int pi = partition(arr, low, high);
+			
+			//Sort the elements before and after partition separately
+			quickSort(arr, low, pi-1);
+			quickSort(arr, pi+1, high);
+		}
+	}
+	public static int partition(Film[] arr, int low, int high) {
+		//pivot will be based on the the last element
+		float pivot = (float)arr[high].ratio();
+		//Smaller element and also indicates the right position of pivot after swaps
+		int i = (low-1);
+		
+		//Iterating from left to right until pivot
+		for(int j=low; j<=high-1; j++) {
+			//if current ratio smaller than the pivot ratio
+			if((float)arr[j].ratio()<pivot) {
+				//increment index of smaller element
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i+1, high); //last thing update pivot location to have
+		//elements greater than pivot on the right hand and smaller to it's left hand
+		return (i+1);
+	}
+
+	//To swap given indexes two array elements
+	private static void swap(Film[] arr, int i, int j) {
+		Film temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	/*Helper method to see how many movies in sorted movies could be discounted from limit*/
+	public static int discountLength(float lim) {
+		int lastIndex = films.length-1;
+		int count=0;
+		while(lim>0) {
+			lim = lim-(float)(films[lastIndex].getLength());
+			lastIndex--;
+			count++;
+		}
+		if(lim<0) {
+			count--;
+		}
+		return count;
+	}
+
+	
 }
 
 class Film implements Comparable<Object>{
@@ -371,6 +458,12 @@ class Film implements Comparable<Object>{
 		}else {
 			return 1;
 		}
+	}
+	
+	/*Part 3*/
+	/*To find the ratio of the movie*/
+	public double ratio() {
+		return (float)rating/length;
 	}
 
 	@Override
